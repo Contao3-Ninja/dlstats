acdb44f8a7a99170669db15a8fc2d468f13b20a2
Glen Langer <contao.modul@glen-langer.de>
2013-03-19 00:29:57 +0100

    Detail-Link, Bild alt-Tag ueber Sprachvariable

--- languages/de/tl_dlstatstatistics_stat.php
+++ languages/de/tl_dlstatstatistics_stat.php
@@ -31,8 +31,9 @@ $GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['footer']       = '';
 
 $GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['wrong_parameter'] = 'Fehlerhafte oder falsche Parameter';
 
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']   = "Datum / Uhrzeit";
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']       = "IP-Adresse / Domain";
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username'] = "Benutzername";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['showRecord'] = 'Details des Datensatzes anzeigen';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']     = "Datum / Uhrzeit";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']         = "IP-Adresse / Domain";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username']   = "Benutzername";
 
 ?>
--- languages/en/tl_dlstatstatistics_stat.php
+++ languages/en/tl_dlstatstatistics_stat.php
@@ -31,8 +31,9 @@ $GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['footer']       = '';
 
 $GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['wrong_parameter'] = 'Faulty or incorrect parameter';
 
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']   = "Date / Time";
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']       = "IP address / Domain";
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username'] = "Member name";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['showRecord'] = 'Show the details of the record';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']     = "Date / Time";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']         = "IP address / Domain";
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username']   = "Member name";
 
 ?>
