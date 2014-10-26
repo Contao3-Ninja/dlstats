ab94a539948b827741d595a3eb48500969c421be
Glen Langer <contao.modul@glen-langer.de>
2014-10-26 22:24:00 +0100

    Transifex Update

--- languages/ja/tl_dlstatstatistics_stat.php
+++ languages/ja/tl_dlstatstatistics_stat.php
@@ -8,39 +8,40 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-07-01T23:26:10+02:00
+ * last-updated: 2014-09-13T21:54:55+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['aliasnotfound']        = 'エイリアスがありません';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['browserlang']          = 'ブラウザーの言語';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['clientside']           = 'クライアント側';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['downloads']            = 'ダウンロード';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['downloads_top20']      = 'ダウンロードの回数の上位%s';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['filename']             = 'ファイル名';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['hostalias']            = 'ホスト / ページのエイリアス';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']                   = 'IPアドレス / ドメイン';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['last_20_downloads']    = '最近のダウンロードの上位%s';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['last_download']        = '最近のダウンロード';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['modname']              = 'ダウンロードの統計モジュール';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['name']                 = 'ダウンロードの統計';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['no_data']              = '統計情報はありません。';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['period']               = '期間';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['reset']                = 'リセット';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['serverside']           = 'サーバー側';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['showRecord']           = 'レコードの詳細を表示';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['startdate']            = '統計の開始';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_activated']     = '有効';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_anonymization'] = '匿名化';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_counting']      = 'ダウンロードの計数';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_deactivated']   = '無効';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_detailed']      = '詳細なログ';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['total_dl']             = 'ダウンロードの総計';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['total_dl_month']       = '過去4か月のダウンロードの回数';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']               = '日付 / 時刻';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username']             = 'メンバー名';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['wrong_parameter']      = '不完全または正しくないパラメーター';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero']                 = '統計のカウンターをゼロにリセットします。';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_confirm']         = '統計のカウンターを本当にリセットしますか?';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_confirm_single']  = '本当にこのカウンターを削除しますか?';
-$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_single']          = 'このカウンターを削除。';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['aliasnotfound']         = 'エイリアスがありません';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['browserlang']           = 'ブラウザーの言語';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['clientside']            = 'クライアント側';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['downloads']             = 'ダウンロード';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['downloads_top20']       = 'ダウンロードの回数の上位%s';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['filename']              = 'ファイル名';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['hostalias']             = 'ホスト / ページのエイリアス';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['ip']                    = 'IPアドレス / ドメイン';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['last_20_downloads']     = '最近のダウンロードの上位%s';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['last_30_calendar_days'] = '最近%s日間の1日辺りのダウンロード';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['last_download']         = '最近のダウンロード';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['modname']               = 'ダウンロードの統計モジュール';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['name']                  = 'ダウンロードの統計';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['no_data']               = '統計情報はありません。';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['period']                = '期間';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['reset']                 = 'リセット';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['serverside']            = 'サーバー側';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['showRecord']            = 'レコードの詳細を表示';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['startdate']             = '統計の開始';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_activated']      = '有効';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_anonymization']  = '匿名化';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_counting']       = 'ダウンロードの計数';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_deactivated']    = '無効';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['status_detailed']       = '詳細なログ';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['total_dl']              = 'ダウンロードの総計';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['total_dl_month']        = '過去4か月のダウンロードの回数';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['tstamp']                = '日付 / 時刻';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['username']              = 'メンバー名';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['wrong_parameter']       = '不完全または正しくないパラメーター';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero']                  = '統計のカウンターをゼロにリセットします。';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_confirm']          = '統計のカウンターを本当にリセットしますか?';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_confirm_single']   = '本当にこのカウンターを削除しますか?';
+$GLOBALS['TL_LANG']['tl_dlstatstatistics_stat']['zero_single']           = 'このカウンターを削除。';
 
