30b75745e84a2a65d4a0abb0f9618af5cb28673c
bibo <Glen.Langer@T-Systems.com>
2015-03-05 11:22:05 +0100

    Fixed #65 - Boolean should be compared strictly

--- classes/ModuleDlstatsStatisticsHelper.php
+++ classes/ModuleDlstatsStatisticsHelper.php
@@ -55,7 +55,7 @@ class ModuleDlstatsStatisticsHelper extends \BackendModule
      */
     public static function getInstance()
     {
-        if (self::$instance == null)
+        if (self::$instance === null)
         {
             self::$instance = new ModuleDlstatsStatisticsHelper();
         }
